32ed58056c2a19f2c4ced7110b5dcf108c73c6e5
wangjun <wangjun@wayonsys.com>
2019-05-09 09:14:51 +0800

    数据库配置

--- redis_pt/src/main/java/com/nott/redis_pt/user/entity/User.java
+++ redis_pt/src/main/java/com/nott/redis_pt/user/entity/User.java
@@ -12,32 +12,32 @@ public class User {
 
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Id
-    @Column(name = "id")
+    @Column(name = "id", length = 32)
     @Getter
     @Setter
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 32)
     @Getter
     @Setter
     private String name;
 
-    @Column(name = "age")
+    @Column(name = "age", length = 4)
     @Getter
     @Setter
     private Integer age;
 
-    @Column(name = "delete")
+    @Column(name = "delete", length = 2)
     @Getter
     @Setter
     private boolean delete = false;
 
-    @Column(name = "create_date")
+    @Column(name = "create_date", length = 16)
     @Getter
     @Setter
     private Date createDate = new Date();
 
-    @Column(name = "modify_date")
+    @Column(name = "modify_date", length = 16)
     @Getter
     @Setter
     private Date modifyDate = new Date();
