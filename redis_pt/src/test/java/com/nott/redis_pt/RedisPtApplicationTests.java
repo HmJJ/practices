49e8d09ccf6754afed866ff56d76fbee8ad24029
wangjun <nottwang@foxmail.com>
2019-07-11 16:35:58 +0800

    update

--- redis_pt/src/test/java/com/nott/redis_pt/RedisPtApplicationTests.java
+++ redis_pt/src/test/java/com/nott/redis_pt/RedisPtApplicationTests.java
@@ -5,12 +5,26 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class RedisPtApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Properties initPro = new Properties(System.getProperties());
+		System.out.println(initPro.getProperty("file.encoding"));
+		String str = new String("asdf 啊手动阀手动阀 a75&%");
+		try {
+			System.out.println(new String(str.getBytes("GBK")));
+			System.out.println(new String(str.getBytes("UTF-8")));
+			System.out.println(new String(str.getBytes("ISO8859-1")));
+			System.out.println(new String(str.getBytes("gb2312")));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
