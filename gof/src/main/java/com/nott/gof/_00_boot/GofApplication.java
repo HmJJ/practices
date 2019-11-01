13db69ac70e42261afdf570acd4cf6d793c2f3b2
wangjun <nottwang@foxmail.com>
2019-11-01 16:38:08 +0800

    update
    ffmpeg流服务搭建，直播demo

--- gof/src/main/java/com/nott/gof/_00_boot/GofApplication.java
+++ gof/src/main/java/com/nott/gof/_00_boot/GofApplication.java
@@ -4,12 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @ComponentScan({"com.nott.gof"})
 @SpringBootApplication
 public class GofApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GofApplication.class, args);
+//		SpringApplication.run(GofApplication.class, args);
+
+
+		String dateStr = "2019-10-29T23:48:19";
+		dateStr = dateStr.replace("T", " ");
+		DateTimeFormatter d = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime ldt = LocalDateTime.parse(dateStr, d);
+		System.out.println(ldt);
+
 	}
 
 }
