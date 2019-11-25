a7d30a1c69ca6d0100ef616b26001ad88c58ad29
wangjun <nottwang@foxmail.com>
2019-11-25 18:47:13 +0800

    update

---
+++ python/calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+a=int(input("请输入第一个参数a: "))
+b=int(input("请输入第二个参数b: "))
+
+print('a + b = ', a + b)
+print("a - b = ", a - b)
+print('a x b = ', a * b)
+print("a / b = ", a / b)
