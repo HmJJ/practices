dd2f110d43382d30e2d290abe9f2e41349341aa2
wangjun <nottwang@foxmail.com>
2020-01-14 18:38:55 +0800

    update

--- sort/src/main/java/com/nott/sort/service/SortService.java
+++ sort/src/main/java/com/nott/sort/service/SortService.java
@@ -28,12 +28,12 @@ public class SortService {
 
         String type = (String) map.get("type");
         if (StringUtils.isEmpty(type)) {
-            return "请选择一种排序方式：[bubble, selection, insertion, shell]";
+            return "请选择一种排序方式：[bubble, selection, insertion, shell, merge, quick, heap, counting, bucket, radix]";
         }
         int arr[] = {9, 2, 0, 3, 7, 4, 6, 5};
         Sort sort = sortFactory.getService(type);
         if (sort == null) {
-            return "请输入正确的排序方式：[]";
+            return "请输入正确的排序方式：[bubble, selection, insertion, shell, merge, quick, heap, counting, bucket, radix]";
         }
         try {
             sort.sort(arr);
